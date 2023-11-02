9b8748f0d36940c3599aceb041399b6cbb3f9f9f
jcw0525 <53311779+jcw0525@users.noreply.github.com>
2023-11-02 18:02:38 -0400

    mvp condition: pending readme
    functional exercise creation, play, timing, scoring - moderate dummy proofing; pending readme

--- Project 2/Project 2/index.js
+++ Project 2/Project 2/index.js
@@ -6,7 +6,13 @@ var longitudes = [];
 var routeIndex = 0;
 var currentlat = 0;
 var currentlng = 0;
+var currenthdg = 0;
+var currentptc = 0;
 var bootup = 1;
+var totalscore = 0.0;
+var partscore = 0.0;
+var time = 0;
+var timer;
 
 function initialize() {
     const default_fenway = { lat: 42.345573, lng: -71.098326 };
@@ -14,6 +20,8 @@ function initialize() {
     if (bootup) {
         this.currentlat = 42.345573;
         this.currentlng = -71.098326;
+        this.currenthdg = 0;
+        this.currentptc = 0;
         document.getElementById("loadCheckpoint").hidden = true;
         bootup = 0;
     }
@@ -27,8 +35,8 @@ function initialize() {
         {
             position: location,
             pov: {
-                heading: 34,
-                pitch: 10,
+                heading: this.currenthdg,
+                pitch: this.currentptc,
             },
         },
     );
@@ -37,6 +45,8 @@ function initialize() {
     panorama.addListener("position_changed", () => {
         this.currentlat = panorama.getPosition().lat();
         this.currentlng = panorama.getPosition().lng();
+        this.currenthdg = panorama.getPov().heading;
+        this.currentptc = panorama.getPov().pitch;
 
         //check if close enough to next position to trigger next direction
         //implement haversine formula
@@ -54,12 +64,17 @@ function initialize() {
             this.routeIndex++;
             this.initialize();
             document.getElementById("output").innerHTML = this.directions[this.routeIndex];
+            this.totalscore += this.partscore;
+            this.partscore = 5000.0 / (this.directions.length-1);
             if (this.directions[this.routeIndex] == "DONE") {
-                document.getElementById("output").innerHTML = "You Scored XXX pts in XXX seconds! Congrats!";
+                document.getElementById("output").innerHTML = "You Scored " + Math.round(this.totalscore) + "pts in " + this.time + " seconds! Congrats!";
+                document.getElementById("timer").hidden = true;
                 document.getElementById("load").hidden = false;
                 document.getElementById("inputs").hidden = false;
                 document.getElementById("toggleMap").hidden = false;
                 document.getElementById("loadCheckpoint").hidden = true;
+                document.getElementById("outputs").style.cssFloat = "left";
+                clearTimeout(this.timer);
             }
         }
     });
@@ -78,12 +93,24 @@ function loadExercise() {
     document.getElementById("map").hidden = true;
 
     //load positions and directions
-    const data = document.getElementById("exercise").value.split("~");
-    for (let i = 0; i < data.length; i += 3)
-    {
-        this.latitudes.push(parseFloat(data[i]));
-        this.longitudes.push(parseFloat(data[i + 1]));
-        this.directions.push(data[i + 2]);
+    try {
+        const data = document.getElementById("exercise").value.split("~");
+        for (let i = 0; i < data.length; i += 3) {
+            if (!parseFloat(data[i]) || !parseFloat(data[i + 1]))
+                throw Exception();
+            this.latitudes.push(parseFloat(data[i]));
+            this.longitudes.push(parseFloat(data[i + 1]));
+            this.directions.push(data[i + 2]);
+        }
+    }
+    catch {
+        document.getElementById("exercise").value = "Bad instruction set; please try again";
+        this.latitudes = [];
+        this.longitudes = [];
+        this.directions = [];
+        this.bootup = 1;
+        initialize();
+        return;
     }
 
     //display current position and direction
@@ -91,15 +118,23 @@ function loadExercise() {
     this.initialize();
     document.getElementById("output").innerHTML = this.directions[this.routeIndex];
 
+    document.getElementById("timer").hidden = false;
     document.getElementById("exercise").value = "";
     document.getElementById("load").hidden = true;
     document.getElementById("inputs").hidden = true;
     document.getElementById("loadCheckpoint").hidden = false;
+    document.getElementById("outputs").style.cssFloat = "right";
+
+    this.time = 0;
+    this.score = 0.0;
+    this.partscore = 5000.0 / this.directions.length;
+    this.timer = window.setInterval(update, 1000);
 }
 
 //loads most recent checkpoint
 function loadCheckpoint() {
     this.initialize();
+    this.partscore *= .66667;
 }
 
 //adds position and direction pair to output; "DONE" if final position
@@ -111,6 +146,15 @@ function addDirection() {
 //adds eof to instruction set
 function saveExercise() {
     document.getElementById("output").innerHTML += "<br>" + this.currentlat + "~" + this.currentlng + "~DONE";
+}
+
+//updates everysecond
+function update() {
+    this.time += 1;
+    this.partscore *= .995;
+    document.getElementById("timeDisplay").innerHTML = "Current Time:<br>" + this.time +
+        " seconds<br>Current Points Available:<br>" + Math.round(this.partscore) +
+        "<br>Total Points Scored:<br>" + Math.round(this.totalscore);
 }
 
 
